61ed667ca4573c0a97813f2fbe63f7d691c9faac
Lorenzo Salvio <salviolorenzo@gmail.com>
2018-11-02 10:45:53 -0400

    adds module export to nother file

--- hello.js
+++ hello.js
@@ -1,1 +1,13 @@
-console.log(`Hello node!`);
+function hello(whom) {
+    console.log(`Hello ${whom}!`);
+}
+
+function goodbye(whom) {
+    console.log(`Goodbye ${whom}.`);
+}
+
+// Export the hello function
+module.exports = {
+    'hello': hello,
+    'gooodbye': goodbye
+};
---
+++ index.js
@@ -0,0 +1,5 @@
+// import the hello function
+const greeting = require('./hello');
+
+greeting.hello('friend');
+greeting.gooodbye('loser');
