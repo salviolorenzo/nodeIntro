568514eba2cab9f51ac8394861632d4e307e12fc
Lorenzo Salvio <salviolorenzo@gmail.com>
2018-11-02 11:37:50 -0400

    adds cowsay module to index

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 // import the hello function
 const { hello, goodbye } = require('./hello');
 
+const cowsay = require('cowsay');
+
+console.log(cowsay.say({
+    text: 'hey',
+    T: 'W'
+}));
+
 hello('friend');
 goodbye('loser');
 
